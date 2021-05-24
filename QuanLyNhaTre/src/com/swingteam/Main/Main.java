8406e5a36b5cc62219302ba7925aeeea7a09e2e1
MinhHieu259 <mhieu7252@gmail.com>
2021-05-24 15:45:19 +0700

    HieuAdd

--- QuanLyNhaTre/src/com/swingteam/Main/Main.java
+++ QuanLyNhaTre/src/com/swingteam/Main/Main.java
@@ -7,7 +7,6 @@ import com.swingteam.views.MainJFrame;
 
 public class Main {
     public static void main(String[] args) {
-//        new DangNhapFrame().setVisible(true);
         new MainJFrame().setVisible(true);
     }
 }
